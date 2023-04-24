2191d577cb86cd0fd7ff77dea935c7c07232165f
Amruthaka <131552805+Amruthaka@users.noreply.github.com>
2023-04-24 13:07:09 +0530

    Add files via upload

---
+++ Printoddnumbers.java
@@ -0,0 +1,19 @@
+package week1.day2;
+
+public class Printoddnumbers {
+
+	public static void main(String[] args) {
+	
+		int num = 20;
+		
+		for (int i = 1; i<=20; i++) {
+			
+			if (i%2!=0)
+				
+				System.out.println(i);
+			
+		}
+
+	}
+
+}
