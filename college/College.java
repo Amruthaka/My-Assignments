9e5f73384021e1805f77564c5811a063913eb9ee
Amruthaka <131552805+Amruthaka@users.noreply.github.com>
2023-05-07 01:43:20 +0530

    Add files via upload

---
+++ college/College.java
@@ -0,0 +1,19 @@
+package org.college;
+
+public class College {
+	
+	public void collegeName() {
+		
+		System.out.println("Ethiraj College");
+	}
+	
+	public void collegeCode() {
+		
+		System.out.println("0025");
+	}
+
+	public void collegeRank() {
+		
+		System.out.println("10");
+	}
+}
